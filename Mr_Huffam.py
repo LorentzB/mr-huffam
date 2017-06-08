b2a8bbbb32d42041331282da58c44b4a7c538907
LorentzB <xb2133@columbia.edu>
2017-06-08 01:10:02 -0400

    for test

--- Mr_Huffam.py
+++ Mr_Huffam.py
@@ -1,14 +1,22 @@
+"""
+Mr. Huffam and his newspaper.
+
+This is the discription.
+"""
 from bs4 import BeautifulSoup
 import requests
 import time
 import os
 import logging
 
-def Get_News():
+
+def get_news():
+    """Get the url of the latest news."""
     news_url = os.environ.get('BLOG_URL')
     news_headers = {'user-agent': os.environ.get('USER-AGENT')}
 
-    news_page = BeautifulSoup(requests.get(news_url, headers=news_headers).content, 'html.parser')
+    news_page = BeautifulSoup(
+        requests.get(news_url, headers=news_headers).content, 'html.parser')
 
     list_news = news_page.find('div', 'container blog-index')
     latest_news = list_news.article
@@ -19,37 +27,45 @@ def Get_News():
 
     return link, title, content
 
-def Get_Cover(link):
+
+def get_cover(link):
+    """Get the cover image of the news."""
     headers = {'user-agent': os.environ.get('USER-AGENT')}
-    page = BeautifulSoup(requests.get(link, headers=headers).content, 'html.parser')
+    page = BeautifulSoup(requests.get(link, headers=headers).content,
+                         'html.parser')
 
     news_content = page.find('div', 'entry')
-    cover = news_content.a['href']
+    cover = news_content.img['src']
     return cover
 
 
-def Mr_Huffam(link, title, content, old_link):
+def mr_huffam(link, title, content, old_link):
+    """Run Main function."""
     headers = {'Content-Type': 'application/json'}
     if old_link != link:
-        cover = Get_Cover(link)
-        news = {'content': 'The breaking news is in print now; come and read, delicious friends. @here \n \n' '**'+title+'**',
-                'embeds':[
-                    {'title':title, 'description':content, 'url':link, 'image':{'url':cover}}
+        cover = get_cover(link)
+        news = {'content': 'The breaking news is in print now; come and read,\
+         delicious friends. \n \n' '**'+title+'**',
+                'embeds': [
+                    {'title': title, 'description': content, 'url': link,
+                     'image': {'url': cover}}
                         ]
                 }
         r = requests.post(os.environ.get('HUFFAM'), headers=headers, json=news)
         return True
     return False
 
+
 if __name__ == "__main__":
-    old_link = os.environ.get('PREVIOUS_NEWS_LINK')
+    # link, title, content = get_news()
+    # old_link = link
+    old_link = ''
     while True:
-        link, title, content = Get_News()
-        have_news = Mr_Huffam(link, title, content, old_link)
+        link, title, content = get_news()
+        have_news = mr_huffam(link, title, content, old_link)
         if have_news:
-            logging.warning('previous news: %s' %old_link)
-            logging.warning('latest news: %s' %link)
+            logging.warning('previous news: %s' % old_link)
+            logging.warning('latest news: %s' % link)
             old_link = link
-            os.environ['PREVIOUS_NEWS_LINK'] = old_link
-        logging.warning('Mr. Huffam is working hard')
+        print('Mr. Huffam is working hard')
         time.sleep(60*20)
